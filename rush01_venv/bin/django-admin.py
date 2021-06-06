e73e0dbdb6137ca4607cecb70db0976818f36c5c
Hyunja27 <123gk@naver.com>
2021-06-06 23:42:43 +0900

    06/06_23:41_initial_info_making

--- rush01_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/spark/Box_main_cursus/11_python_piscine/rush01/rush01_venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
